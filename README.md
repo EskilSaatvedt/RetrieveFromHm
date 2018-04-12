70e343cc6c592ec94a5c734bbcb863c106f2a0c8
eskil.saatvedt <eskil.saatvedt@bouvet.no>
2018-04-12 11:42:46 +0200

    Tuning error handling

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # RetrieveFromHm
 Retreive files from Handyman
+
+This is using the COM object from GSHandyman to retrieve files by HSDocumentID as a Microsoft 4.6 Framework MVC with minimum of code.
+
--- RetrieveFromHm/Controllers/DocumentController.cs
+++ RetrieveFromHm/Controllers/DocumentController.cs
@@ -8,11 +8,17 @@ namespace RetrieveFromHm.Controllers
 {
 	public class DocumentController : Controller
 	{
+		/// <summary>
+		/// http://ip:port/Document/Download/[HSDocumentID] will return the Document if it exisit in the Handyman 
+		/// The Client ID is set in the Web.config file
+		/// </summary>
+		/// <param name="id">HSDocumentID for give document</param>
+		/// <returns>The File in question, return 400 if not found or some error occured</returns>
 		// GET: Document
 		public ActionResult Download(int id = 0)
 		{
 			if (id == 0)
-				throw new HttpException(400, "Bad Request");
+				throw new HttpException(404, "Bad Request");
 			// clientId is set in Web.config->applicationSettings
 			var clientId = RetrieveFromHm.Properties.Settings.Default.clientId;
 
@@ -20,10 +26,19 @@ namespace RetrieveFromHm.Controllers
 			var handyman = new XML();
 			handyman.set_ClientId(clientId);
 
+			var filePath = "";
 			// Exporting the document out of Handyman
-			var filePath = handyman.GetOrderDocument(id, 1);
+			try
+			{
+				filePath = handyman.GetOrderDocument(id, 1);
+			}
+			catch
+			{
+				throw new HttpException(404, "Unable to retrieve the file from Handyman");
+			}
+			
 			if (!string.IsNullOrEmpty(filePath) && !System.IO.File.Exists(filePath))
-				throw new HttpException(400, "Document error, unable to export the file");
+				throw new HttpException(404, "Document error, unable to export the file");
 			var mimeType = MimeTypesMap.GetMimeType(filePath);
 
 			return new FilePathResult(filePath, mimeType);
